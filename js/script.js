c8933d850bf5f054656a82d0b609e6954b6c5e8c
obadiaH123 <benard.bett@student.moringaschool.com>
2021-07-31 17:14:57 +0300

    adding validation to js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,41 @@
       this.toppings=toppings;
       this.total=total;
   }
+
+//user interface logic
+//the form
+$(document).ready(function(){
+    $("button.proceed").click(function(event){
+        $("button.cancel").hide();
+        let pname = $(".name option:selected").val();
+        let psize = $("#size option:selected").val();
+        let pcrust = $("#crust option:selected").val();
+        let ptopping = [];
+        $.each($("input[name='toppings']:checked"), function() {
+            ptopping.push($(this).val());
+       });
+    console.log(ptopping.join(", "));
+
+    switch (psize) {
+        case "0":
+            price = 0;
+            break;
+        case "large":
+            price = 1200;
+            console.log(price);
+            break;
+        case "medium":
+            price = 1000;
+            console.log("The Price is " + price);
+            break;
+        case "Small":
+            price = 700;
+            console.log(price);
+            break;
+        default:
+            console.log("error");
+    
+        default:
+            break;
+    }
+});
