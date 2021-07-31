5aebb1e1c577b57689e53578e5a5fc6a70727323
obadiaH123 <benard.bett@student.moringaschool.com>
2021-07-31 18:33:21 +0300

    adding total function

--- js/script.js
+++ js/script.js
@@ -85,9 +85,17 @@ $(document).ready(function(){
     }
     total = price + crust_price + topping_value;
     console.log(total);
+    let checkoutTotal = 0;
+    checkoutTotal = checkoutTotal + total;
+
+    $("#pizzaname").html($(".name option:selected").val());
+    $("#pizzasize").html($(".size option:selected").val());
+    $("#pizzacrust").html($(".crust option:selected").val());
+    $("#pizzatopping").html(ptopping.join(", "));
+    $("#totals").html(total);
             
         });
-    }
+    
 
 
 });
